fbdc43c9cad999a8e13ffb8064c6cf26117fd3f3
Fernando Rodriguez Martinez <Fernandormartinez21@gmail.com>
2026-05-19 20:37:14 -0700

    Customized components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import {Button} from '@/components/ui/button';
 export default function Home() {
   return (
     <div className='flex-gap-4'>
-      <Button>Primary</Button>
-      <Button variant='secondary'>Secondary</Button>
+      <Button size='xs'>Primary</Button>
+      <Button variant='secondary' size='lg'>Secondary</Button>
       <Button variant='destructive'>Destructive</Button>
       <Button variant='ghost'>Ghost</Button>
-      <Button variant='link'>Link</Button>
+      <Button variant='muted'>Muted</Button>
       <Button variant='outline'>Outline</Button>
+      <Button variant='teritary'>Teritary</Button>
     </div>
   );
 }
