c3c25b34f85905ac18a62f43f6891dacada2dbcc
Fernando Rodriguez Martinez <Fernandormartinez21@gmail.com>
2026-05-21 15:31:26 -0700

    feat(auth): initialize appwrite backend and implement core authentication
    - Configure Admin Appwrite client setup
    
    - Implement /login and /register endpoints using Hono
    
    - Integrate Zod payload schema validations
    
    - Deploy secure HTTP-only session cookie authentication storage

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -107,7 +107,7 @@ export const SignUpCard = () => {
               />
 
               <Button disabled={false} size="lg" className="w-full">
-                  Sign In
+                  Register
               </Button>
           </form>
         </Form>
